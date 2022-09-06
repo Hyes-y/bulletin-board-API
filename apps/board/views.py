52e867721446be7e67eb5816c3cca30c6c1f42a4
Hyes-y <gptjs7970@naver.com>
2022-09-07 06:12:59 +0900

    feat: 게시판 CRUD 구현 - create, update, list, retrieve 는 PostViewSet에 구현(각각 필요한 로직은 serializer에 추가) - PostViewSet의 destroy() 는 사용하지 않음 - PostDeleteViewSet은 UpdateModelMixin만 상속(update 방식으로 삭제) - password만 입력으로 받기때문에 partial=True

--- apps/board/views.py
+++ apps/board/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+# django rest api
+from rest_framework import viewsets, mixins, status
+from rest_framework.response import Response
+# local modules
+from .models import Post
+from .serializers import PostSerializer, PostDeleteSerializer
 
-# Create your views here.
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    게시판 글 CRU ViewSet
+    """
+    queryset = Post.objects.filter(is_deleted=False)
+    serializer_class = PostSerializer
+
+    def destroy(self, request, pk=None):
+        """
+        Delete 메소드는 허용하지 않습니다.
+        """
+        response = {'ERROR': 'Delete 메소드는 허용하지 않습니다.'}
+        return Response(response, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    # def perform_create(self, serializer):
+    #     serializer.save()
+
+
+class PostDeleteViewSet(mixins.UpdateModelMixin,
+                        viewsets.GenericViewSet):
+
+    queryset = Post.objects.all()
+    serializer_class = PostDeleteSerializer
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', True)
+        request.data['is_deleted'] = True
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        response = {'Success Deleted': '성공적으로 삭제되었습니다.'}
+        return Response(response, status=status.HTTP_204_NO_CONTENT)
+
+
+

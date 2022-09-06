e460efc9cc5650f43a25c995e0ab30f12d0006d3
Hyes-y <gptjs7970@naver.com>
2022-09-07 05:06:15 +0900

    fix: verbose_name, Password field 변경 - verbose_name 속성으로 다시 적용 - models.BinaryField 를 사용했으나 serializer에서 지원하지 않아 데이터 저장이 안됨 - models.CharField로 변경

--- apps/board/models.py
+++ apps/board/models.py
@@ -7,18 +7,19 @@ class Post(models.Model):
     정렬 - 최신순
     Input - title, content, author, password
     """
-    created_at = models.DateTimeField('등록 날짜', auto_now_add=True)
-    updated_at = models.DateTimeField('수정 날짜', auto_now=True)
+    created_at = models.DateTimeField(verbose_name='등록 날짜', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='수정 날짜', auto_now=True)
 
-    title = models.CharField('제목', max_length=20)
-    content = models.CharField('본문', max_length=200)
+    title = models.CharField(verbose_name='제목', max_length=20)
+    content = models.CharField(verbose_name='본문', max_length=200)
     # TextField 의 max_length 는 form area 에만 영향을 주고 model 혹은 database 레벨 에서는 영향을 주지 않는다.
     # 길이 제한이 필요하면 CharField 를 써야 한다.
     # 무얼 쓰면 좋을까?
-    # content = models.TextField('본문')
+    # content = models.TextField(verbose_name='본문')
 
-    author = models.CharField('글쓴이', max_length=10)
-    password = models.BinaryField()
+    author = models.CharField(verbose_name='글쓴이', max_length=10)
+    password = models.CharField(verbose_name='비밀번호', max_length=255)
+    is_deleted = models.BooleanField(verbose_name="삭제 여부", default=False)
 
     def __str__(self):
         return self.title

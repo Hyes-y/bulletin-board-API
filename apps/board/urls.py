d592cc34bcc0dcfd1cc1c5491c5d2a06f57a1559
Hyes-y <gptjs7970@naver.com>
2022-09-07 06:16:38 +0900

    feat: delete용 url 추가 - posts-delete/:id 로 연결

--- apps/board/urls.py
+++ apps/board/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet
+from .views import PostViewSet, PostDeleteViewSet
 
 router = DefaultRouter()
-router.register(r'posts', PostViewSet, basename='post')
+router.register(r'posts', PostViewSet, basename='posts')
+router.register(r'posts-delete', PostDeleteViewSet, basename='posts-delete')
 
 urlpatterns = [
     path('', include(router.urls)),

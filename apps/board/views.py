93bc526dd3880e5d2f02d0afc1a6e6e40dfc322e
Hyes-y <gptjs7970@naver.com>
2022-09-07 10:51:42 +0900

    docs: 주석 수정

--- apps/board/views.py
+++ apps/board/views.py
@@ -8,28 +8,30 @@ from .serializers import PostSerializer, PostDeleteSerializer
 
 class PostViewSet(viewsets.ModelViewSet):
     """
-    게시판 글 CRU ViewSet
+    게시글 등록, 수정, 조회 ViewSet
     """
     queryset = Post.objects.filter(is_deleted=False)
     serializer_class = PostSerializer
 
     def destroy(self, request, pk=None):
-        """
-        Delete 메소드는 허용하지 않습니다.
-        """
-        response = {'ERROR': 'Delete 메소드는 허용하지 않습니다.'}
+        """ DELETE 메소드는 허용하지 않습니다. """
+        response = {'ERROR': 'DELETE 메소드는 허용하지 않습니다.'}
         return Response(response, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
     # def perform_create(self, serializer):
     #     serializer.save()
 
 
 class PostDeleteViewSet(mixins.UpdateModelMixin,
                         viewsets.GenericViewSet):
-
+    """
+    게시글 삭제 ViewSet
+    """
     queryset = Post.objects.all()
     serializer_class = PostDeleteSerializer
 
     def update(self, request, *args, **kwargs):
+        """ 게시글 삭제 처리 """
         partial = kwargs.pop('partial', True)
         request.data['is_deleted'] = True
         instance = self.get_object()
@@ -39,6 +41,4 @@ class PostDeleteViewSet(mixins.UpdateModelMixin,
 
         response = {'Success Deleted': '성공적으로 삭제되었습니다.'}
         return Response(response, status=status.HTTP_204_NO_CONTENT)
-
-
 
